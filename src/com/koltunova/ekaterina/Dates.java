85c347ecd42bff69d021da7ae20a0c8b69f3bbcf
Kateriina <blondinkova@mail.ru>
2022-02-27 23:55:34 +0300

    added cron

--- src/com/koltunova/ekaterina/Dates.java
+++ src/com/koltunova/ekaterina/Dates.java
@@ -1,13 +1,167 @@
 package com.koltunova.ekaterina;
 
-import java.util.List;
+import java.util.*;
 
 public class Dates implements DatesToCronConverter{
 
     public Dates(){}
 
     @Override
-    public String convert(List<String> dates) throws DatesToCronConvertException {
+    public String convert(List<String> list) throws DatesToCronConvertException {
+        List<Format> formats = new ArrayList<>();
+        Collections.sort(list);
+        for (String s : list) { formats.add(new Format(s)); }
+
+        Map<String, List<Format>> mapDayOfMonth = new HashMap();
+        Map<String, List<Format>> mapMonth = new HashMap();
+        Map<String, List<Format>> mapWeekD = new HashMap();
+        Map<String, List<Format>> mapHour = new LinkedHashMap<>();
+
+        for (Format format : formats) {
+            String dayOfWeek = format.getWeekD();
+            if (!mapWeekD.containsKey(dayOfWeek)) mapWeekD.put(dayOfWeek, new ArrayList<>());
+            mapWeekD.get(dayOfWeek).add(format);
+
+            String dayOfMonth = format.getDay();
+            if (!mapDayOfMonth.containsKey(dayOfMonth)) mapDayOfMonth.put(dayOfMonth, new ArrayList<>());
+            mapDayOfMonth.get(dayOfMonth).add(format);
+
+            String month = format.getMonth();
+            if (!mapMonth.containsKey(month)) mapMonth.put(month, new ArrayList<>());
+            mapMonth.get(month).add(format);
+
+            String hour = format.getHour();
+            if (!mapHour.containsKey(hour)) mapHour.put(hour, new ArrayList<>());
+            mapHour.get(hour).add(format);
+        }
+
+        String secSol = SecForm(formats);
+        String minuteSol = MinForm(formats);
+        String hourSol = HourForm(mapHour, formats.size());
+        String dayOfMonthSol = MonthForm(mapDayOfMonth, formats.size());
+        String monthSol = MonthForm(mapMonth, formats.size());
+        String dayOfWeekSol = MonthForm(mapWeekD, formats.size());
+
+        String[] resArr = new String[6];
+
+        resArr[0] = (secSol ==null)?"*": secSol;
+        resArr[1] = (minuteSol ==null)?"*": minuteSol;
+        resArr[2] = (hourSol ==null)?"*": hourSol;
+        resArr[3] = (dayOfMonthSol ==null)?"*": dayOfMonthSol;
+        resArr[4] = (monthSol ==null)?"*": monthSol;
+        resArr[5] = (dayOfWeekSol ==null)?"*": dayOfWeekSol;
+
+        String result = Arrays.toString(resArr)
+                .replace(",","")
+                .replace("[","'")
+                .replace("]","'");
+
+        if (result.equals("'* * * * * *'")) throw new DatesToCronConvertException();
+
+        System.out.println(result);
+        return result;
+    }
+
+    String MonthForm(Map<String, List<Format>> map, int size) {
+        List<Map.Entry<String, List<Format>>> list = new ArrayList<>(map.entrySet());
+        int maxLength = 0;
+
+        for (Map.Entry<String, List<Format>> stringListEntry : list) {
+            int solutionLength = stringListEntry.getValue().size();
+            if (solutionLength > maxLength) {
+                maxLength = solutionLength;
+                if (maxLength > size/2) return stringListEntry.getKey();
+            }
+        }
+        return null;
+    }
+
+    String HourForm(Map <String, List<Format>> map, int size) {
+        List<Map.Entry<String, List<Format>>> listHour = new ArrayList<>(map.entrySet());
+        int firstHour = 25;
+        int timeInterval = 1;
+        List<Format> tempList = new ArrayList<>();
+        String tempStr = "*";
+
+        for (int i = 0; i < listHour.size(); i++) {
+            Map.Entry<String, List<Format>> stringListEntry = listHour.get(i);
+
+            int currentHour = Integer.parseInt(stringListEntry.getKey());
+
+            if (i==0) {
+                firstHour = currentHour;
+                tempList = stringListEntry.getValue();
+                continue;
+            }
+
+            if (currentHour-firstHour==timeInterval) {
+                tempStr = firstHour + "-" + currentHour;
+                tempList.addAll(stringListEntry.getValue());
+                timeInterval++;
+            }
+        }
+
+        if (tempList.size() > size/2) {
+            return tempStr;
+        }
+        else return null;
+    }
+
+    String MinForm(List<Format> formats) {
+        int period = 0;
+        List<Format> tempList = new ArrayList<>();
+        String tempStr = "*";
+
+        for (int i = 1; i < formats.size(); i++) {
+            int firstMinute = Integer.parseInt(formats.get(i-1).getMin());
+            int secondMinute = Integer.parseInt(formats.get(i).getMin());
+
+            if (i==1) {
+                period =  secondMinute - firstMinute;
+                if (period>1) {
+                    tempStr = firstMinute + "/" + period;
+                    tempList = new ArrayList<>();
+                    tempList.add(formats.get(0));
+                    tempList.add(formats.get(1));
+                }
+            }
+
+            if (i>1 && Math.abs(secondMinute - firstMinute) == period) {
+                tempList.add(formats.get(i));
+            }
+            if (tempList.size() > formats.size()/2) {
+                return tempStr;
+            }
+        }
+        return null;
+    }
+
+    String SecForm(List<Format> formats) {
+        int period = 0;
+        List<Format> tempList = new ArrayList<>();
+        String tempStr = "*";
+
+        for (int i = 1; i < formats.size(); i++) {
+            int firstSec = Integer.parseInt(formats.get(i-1).getSec());
+            int secondSec = Integer.parseInt(formats.get(i).getSec());
+
+            if (i==1) {
+                period =  secondSec - firstSec;
+                if (period>1) {
+                    tempStr = firstSec + "/" + period;
+                    tempList = new ArrayList<>();
+                    tempList.add(formats.get(0));
+                    tempList.add(formats.get(1));
+                }
+            }
+
+            if (i>1 && Math.abs(secondSec - firstSec) == period) {
+                tempList.add(formats.get(i));
+            }
+            if (tempList.size() > formats.size()/2) {
+                return tempStr;
+            }
+        }
         return null;
     }
 
--- src/com/koltunova/ekaterina/Main.java
+++ src/com/koltunova/ekaterina/Main.java
@@ -1,9 +1,25 @@
 package com.koltunova.ekaterina;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class Main {
-    public static void main (String[] args) {
+    public static String formatDateByPattern(Date date, String dateFormat) {
+        SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
+        String formatTimeStr = null;
+        if (date != null) {
+            formatTimeStr = sdf.format(date);
+        }
+        return formatTimeStr;
+    }
+
+    public static String getCron(Date date) {
+        String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+        return formatDateByPattern(date, DATE_FORMAT);
+    }
+
+    public static void main (String[] args) throws DatesToCronConvertException {
         DatesToCronConverter dates = new Dates();
         System.out.println(dates.getImplementationInfo());
 
@@ -16,6 +32,10 @@ public class Main {
                 "2022-01-26T08:30:00",
                 "2022-01-26T09:00:00",
                 "2022-01-26T09:30:00");
+        System.out.println("\nПример 1. ");
+        System.out.println(dates1);
+        System.out.println("Cron: ");
+        dates.convert(dates1);
 
         List<String> dates2 = Arrays.asList(
                 "2022-01-24T19:53:00",
@@ -28,7 +48,56 @@ public class Main {
                 "2022-01-24T20:00:00",
                 "2022-01-24T20:01:00",
                 "2022-01-24T20:02:00");
+        System.out.println("\nПример 2.");
+        System.out.println(dates2);
+        System.out.println("Cron: ");
+        dates.convert(dates1);
 
-                System.out.println(dates1);
+        List<String> dates3 = Arrays.asList(
+                "2022-01-24T07:00:00",
+                "2022-01-24T07:00:10",
+                "2022-01-24T07:00:20",
+                "2022-01-24T07:00:30",
+                "2022-01-24T07:00:40",
+                "2022-01-24T07:00:50",
+                "2022-01-24T07:01:00"
+                );
+        System.out.println("\nПример 3.");
+        System.out.println(dates3);
+        System.out.println("Cron: ");
+        dates.convert(dates3);
+
+        List<String> dates4 = Arrays.asList(
+                "2022-01-24T07:10:00",
+                "2022-01-24T07:20:00",
+                "2022-01-24T07:30:00",
+                "2022-01-24T07:40:00",
+                "2022-01-24T07:50:00",
+                "2022-01-24T08:00:00"
+        );
+        System.out.println("\nПример 4.");
+        System.out.println(dates4);
+        System.out.println("Cron: ");
+        dates.convert(dates4);
+
+        List<String> dates5 = Arrays.asList(
+                "2022-02-24T07:10:00",
+                "2022-03-24T07:10:00",
+                "2022-04-24T07:10:00"
+        );
+        System.out.println("\nПример 5.");
+        System.out.println(dates5);
+        System.out.println("Cron: ");
+        dates.convert(dates5);
+
+        List<String> dates6 = Arrays.asList(
+                "2022-02-24T07:10:00",
+                "2022-06-24T07:10:00",
+                "2022-08-24T07:10:00"
+        );
+        System.out.println("\nПример 6.");
+        System.out.println(dates6);
+        System.out.println("Cron: ");
+        dates.convert(dates6);
     }
 }
